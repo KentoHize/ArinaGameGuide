72f0bc978d2a30639f10f5ef36e5d6fc8e38ac1b
日瀬 健人 <kentnewbie@hotmail.com>
2023-04-10 16:24:59 +0800

    T

--- ArinaGameGuide/ArinaGameGuide.js
+++ ArinaGameGuide/ArinaGameGuide.js
@@ -1,15 +1,15 @@
 //import { Doc, Div } from "./js/Ansrabrar.js";
 //import { Riana } from "./js/Characters/Heroes/Riana.js";
 
-export function loadDataIndex(document, indexDiv, contentDiv, pageName, dataName, id1, id2, nameId) {    
-        import(getDataPath(pageName, dataName), { assert: { type: 'json' } }).then(
+export function loadDataIndex(document, indexDiv, contentDiv, pageName, dataName, id1, id2, nameId) {
+    import(getDataPath(pageName, dataName), { assert: { type: 'json' } }).then(
         m => {
             let d = m.default;
             //let doc = new Doc();            
             indexDiv.innerHTML = ``;
             for (let i = 0; i < d.length; i++) {
-                let child = document.createElement(`div`);                
-                let a = document.createElement(`a`);                
+                let child = document.createElement(`div`);
+                let a = document.createElement(`a`);
                 a.href = `#`;
                 a.textContent = d[i][nameId];
                 if (id2)
@@ -22,14 +22,14 @@ export function loadDataIndex(document, indexDiv, contentDiv, pageName, dataName
                 indexDiv.appendChild(child);
                 //let a = Riana.CreateDiv(doc, null, null, null, false, false, doc);
                 //Riana.CreateText(d[i].Name, a);
-                }
-                document.getElementById("index").style.height = window.innerHeight - 200;
+            }
+
             //doc.printTo(document, div);         
         }
     );
 }
 
-export function loadDataContent(div, dataName, id1, id2 = null, params = null) {    
+export function loadDataContent(div, dataName, id1, id2 = null, params = null) {
     //var PData, BRData;
     //var divMain = document.getElementById("main");
     //alert(dataName);
@@ -37,7 +37,7 @@ export function loadDataContent(div, dataName, id1, id2 = null, params = null) {
     let pageParams = `id1=` + id1;
     if (id2)
         pageParams += `&id2=` + id2;
-    pageParams += "$v=" + Math.random();    
+    pageParams += "$v=" + Math.random();
     var xhttp = new XMLHttpRequest();
     xhttp.responseType = 'html';
     xhttp.open("GET", `./` + dataName + ".html?" + pageParams, true);
@@ -46,7 +46,7 @@ export function loadDataContent(div, dataName, id1, id2 = null, params = null) {
             var bodyContent = this.response.substring(this.response.indexOf("<body>") + 6);
             bodyContent = bodyContent.substring(0, bodyContent.lastIndexOf("</body>"));
             //alert(bodyContent);
-            div.innerHTML = bodyContent;    
+            div.innerHTML = bodyContent;
         }
     }
     xhttp.send();
