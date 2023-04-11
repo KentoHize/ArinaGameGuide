66d7089cb6b9ee101dad339ac043d8d49cfd9698
日瀬 健人 <kentnewbie@hotmail.com>
2023-04-11 12:43:18 +0800

    T

---
+++ ArinaGameGuide/js/Devices/ArTranslator.js
@@ -0,0 +1,23 @@
+﻿import { Aritiafel } from "../Aritiafel.js"
+
+export class ArTranslator {
+    #resourceFileObj;
+    State = 0;
+    
+    constructor() {
+        this.State = 1;        
+    }   
+
+    async LoadResourceFile(resourceFile) {
+        this.#resourceFileObj = (await import(resourceFile, { assert: { type: `json` } })).default;
+        this.State = 2;
+    }
+
+    GetString(key) {
+        return this.#resourceFileObj.find(m => m.Key == key).Value;
+    }
+}
+
+if (Aritiafel.Devices == null)
+    Aritiafel.Devices = {};
+Aritiafel.Devices.ArTranslator = ArTranslator;
---
+++ ArinaGameGuide/js/Organizations/RabbitCouriers.js
@@ -0,0 +1,39 @@
+﻿import { Aritiafel } from "../Aritiafel.js"
+import { ArTranslator } from "../Devices/ArTranslator.js"
+
+export class RabbitCouriers {
+
+    static #resourceFileFolder
+    static Translators = [];
+    static DefaultCultureInfo = ``;
+
+    static async RegisterResourcesFilesFolderAndCultureInfo(resourceFilesFolder, cultureInfo) {
+        RabbitCouriers.Translators = [];
+        RabbitCouriers.DefaultCultureInfo = cultureInfo;
+        RabbitCouriers.#resourceFileFolder = resourceFilesFolder;        
+        RabbitCouriers.Translators[RabbitCouriers.DefaultCultureInfo] = new ArTranslator();
+        await RabbitCouriers.Translators[RabbitCouriers.DefaultCultureInfo].LoadResourceFile(`../../${RabbitCouriers.#resourceFileFolder}/${RabbitCouriers.DefaultCultureInfo}.json`);
+    }
+
+    static async RegisterNewCultureInfo(cultureInfo) {
+        RabbitCouriers.Translators[cultureInfo] = new ArTranslator();
+        await RabbitCouriers.Translators[cultureInfo].LoadResourceFile(`../../${RabbitCouriers.#resourceFileFolder}/${cultureInfo}.json`)
+    }
+
+    //Check
+    static UnregisterAll() {
+        delete RabbitCouriers.Translators;
+        RabbitCouriers.Translators = [];
+    }
+
+    static GetMessage(key, cultureInfo = null) {        
+        if (cultureInfo)
+            return RabbitCouriers.Translators[cultureInfo].GetString(key);
+        else
+            return RabbitCouriers.Translators[RabbitCouriers.DefaultCultureInfo].GetString(key);
+    }   
+}
+
+if (Aritiafel.Organizations == null)
+    Aritiafel.Organizations = {};
+Aritiafel.Organizations.RabbitCouriers = RabbitCouriers;
